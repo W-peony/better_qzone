7a328adb7beadee0b241a533efe00627312af810
peony <601664905@qq.com>
2023-01-04 21:29:39 +0800

    delete body

--- Tampermonkey/main.js
+++ Tampermonkey/main.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         QQ空间优化器
+// @name         QQ空间净化器
 // @namespace    http://tampermonkey.net/
 // @version      1.4.2
 // @description  让你的QQ空间变得更加美妙！
 // @author       白芍
-// @match        https://*.qq.com/
+// @match        https://user.qzone.qq.com/*
 // @icon         https://ts1.cn.mm.bing.net/th?id=ODLS.f60b6ee6-1514-4c8b-ad2b-7e1c247e3c27&w=16&h=16&o=6&pid=1.2
 // @license      AGPL-3.0
 // @grant        none
@@ -12,6 +12,10 @@
 
 (() => {
     'use strict';
-
-    console.log(document.cookie)
+    try {
+        let body = document.getElementsByClassName('bg-body');
+        node.parentNode.removeChild(node);
+        console.log(document.cookie);
+    }
+    catch { }
 })();
