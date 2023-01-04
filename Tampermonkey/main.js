9e7ac37db8582d2726bcd61ff28f141a64a16d54
peony <601664905@qq.com>
2023-01-04 22:17:35 +0800

    invisible

--- Tampermonkey/main.js
+++ Tampermonkey/main.js
@@ -7,18 +7,11 @@
 // @match        https://user.qzone.qq.com/*
 // @icon         https://ts1.cn.mm.bing.net/th?id=ODLS.f60b6ee6-1514-4c8b-ad2b-7e1c247e3c27&w=16&h=16&o=6&pid=1.2
 // @license      AGPL-3.0
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (() => {
     'use strict';
-    try {
-        let body = document.getElementsByClassName('bg-body');
-        let childs = body.childNodes;
-        for (let i = 0, len = childs.legnth; i < len; i++) {
-            body.removeChild(childs[i]);
-        }
-        console.log(document.cookie);
-    }
-    catch { }
+    $('.layout-invisible').hide();
+    console.log(document.cookie);
 })();
